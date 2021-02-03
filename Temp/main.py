103f1aa96a937d395310fea9523c8b184787a16b
VincenzoSusso <vincenzosusso@protonmail.com>
2021-02-03 17:27:33 +0100

    Da rivedere struttura

--- Temp/main.py
+++ Temp/main.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 PATTERN_PROTOCOL_REGEX = "http[s]?://"
 ROOT_FOLDER_PATH = "./Data"
 ROOT_URL = "https://www.quattroruote.it/tutte-le-auto/ricerca-per-marca.html"
-N_MAX = 1
+N_MAX = 2
 
 
 def remove_protocol_url(url):
@@ -66,8 +66,6 @@ def save_website(root_folder_path, url_string, relative_path_online, visited_pag
         url = urllib.request.urlopen(url_string)
         soup = BeautifulSoup(url, "html.parser")
 
-        #print("Root folder: ", root_folder_path, "\turl string: ", url_string)
-
         split_url = url_string.split("/")
         if relative_path_online:
             path = split_url[-2] + "/"
@@ -75,24 +73,43 @@ def save_website(root_folder_path, url_string, relative_path_online, visited_pag
             path = url_string.removesuffix(split_url[-1])
 
         #save_html_file(root_folder_path, url_string)
+
+        i = 0
         for link in soup.findAll("a", href=True):
             link_string = str(link["href"])
 
             if path in link_string:
                 if relative_path_online:
                     # Building the full link
-                    #print("link string: ", link_string)
                     link_string = split_url[0] + "//" + split_url[2] + link_string  # todo da rivedere
 
                 if link_string not in visited_pages:
                     # link_string += ".html"
                     visited_pages.add(link_string)
-                    #print(visited_pages)
                     print("Vado qui:", link_string)
+                    i += 1
+
+                    if i > N_MAX:
+                        break
+
+                    pippo(link_string)
                     save_website(root_folder_path, link_string, relative_path_online, visited_pages)
 
     else:
         raise ValueError("The first two values of the function must be strings, the last one must be bool")
+
+def pippo(url_string):
+    if isinstance(url_string, str):
+        url = urllib.request.urlopen(url_string)
+        soup = BeautifulSoup(url, "html.parser")
+
+        for list in soup.findAll("ul", class_="c-featureslist c-featureslist--style1"):
+            for childList in list.findChildren("li"):
+                for childElement in childList.findChildren("strong"):
+                    print(childElement.text)
+
+    else:
+        raise ValueError("")
 
 
 visited_pages = set()
