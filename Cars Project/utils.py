7c9d6548b244f0d79162ac6084ecb6b7c21edb9e
VincenzoSusso <vincenzosusso@protonmail.com>
2021-02-06 17:40:44 +0100

    Eliminati file superflui

--- Cars Project/utils.py
+++
@@ -1,5 +0,0 @@
-def removeSpaces(string):
-    if isinstance(string, str):
-        return string.replace("\n", "").replace("\t", "").replace(" ", "")
-    else:
-        raise ValueError("The object that was passed was not a string")
